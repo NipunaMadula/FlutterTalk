c672f7673937775136ce300476a4a6a67b4a92b1
Nipuna Madula <nipunamadula2001@gmail.com>
2024-12-25 02:25:09 +0530

    Implement Firebase authentication service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential?> signIn(String email, String password) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future<UserCredential?> register(String email, String password) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
